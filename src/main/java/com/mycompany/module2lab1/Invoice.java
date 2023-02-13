0be106b6db25332f661959a97c5a5fa441e7320f
behlt <behlt@DESKTOP-UG0V851>
2023-02-12 23:56:36 -0500

    seco commit

---
+++ src/main/java/com/mycompany/module2lab1/Invoice.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.module2lab1;
+
+/**
+ *
+ * @author behlt
+ */
+public class Invoice {
+    private String partNumber;
+    private String partDescription;
+    private int quantity;
+    private double pricePerItem;
+
+    public Invoice(String partNumber, String partDescription, int quantity, double pricePerItem) {
+        this.partNumber = partNumber;
+        this.partDescription = partDescription;
+        this.quantity = (quantity > 0) ? quantity : 0;
+        this.pricePerItem = (pricePerItem > 0) ? pricePerItem : 0.0;
+    }
+
+    public String getPartNumber() {
+        return partNumber;
+    }
+
+    public void setPartNumber(String partNumber) {
+        this.partNumber = partNumber;
+    }
+
+    public String getPartDescription() {
+        return partDescription;
+    }
+
+    public void setPartDescription(String partDescription) {
+        this.partDescription = partDescription;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = (quantity > 0) ? quantity : 0;
+    }
+
+    public double getPricePerItem() {
+        return pricePerItem;
+    }
+
+    public void setPricePerItem(double pricePerItem) {
+        this.pricePerItem = (pricePerItem > 0) ? pricePerItem : 0.0;
+    }
+
+    public double getInvoiceAmount() {
+        return quantity * pricePerItem;
+    }
+}
